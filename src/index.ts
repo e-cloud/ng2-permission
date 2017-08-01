40a2a484c296785fa6fa1ea8a09235a01692b08f
e-cloud <saintscott119@gmail.com>
2017-08-01 22:24:22 +0800

    fix(index): export permission pipe for proper aot

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,7 @@ export * from './permission/Authorization/PermissionMap';
 export * from './permission/directives/permissionIf.directive';
 export * from './permission/models/Permission';
 export * from './permission/models/Role';
+export * from './permission/pipes/permission.pipe';
 export * from './permission/services/permission-guard.service';
 export * from './permission/stores/PermissionStore';
 export * from './permission/stores/RoleStore';
