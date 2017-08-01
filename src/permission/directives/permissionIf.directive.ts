107f235020599565ba19f7eb2f9a50587e8e27f1
e-cloud <saintscott119@gmail.com>
2017-08-01 16:02:42 +0800

    feat(permissionIf): support then & else block, and external condition

--- src/permission/directives/permissionIf.directive.ts
+++ src/permission/directives/permissionIf.directive.ts
@@ -1,4 +1,4 @@
-import { Directive, Input, OnChanges, OnInit, TemplateRef, ViewContainerRef } from '@angular/core';
+import { Directive, EmbeddedViewRef, Input, OnChanges, OnInit, TemplateRef, ViewContainerRef } from '@angular/core';
 import { Authorization } from '../Authorization/Authorization';
 import { RawPermissionMap } from '../Authorization/PermissionMap';
 import { getRawMap } from '../utils';
@@ -8,18 +8,45 @@ import { getRawMap } from '../utils';
 })
 export class PermissionIfDirective implements OnInit, OnChanges {
     private rawMap: RawPermissionMap = {};
-    private $state: boolean;
+    private _context: PermissionIfContext = new PermissionIfContext();
+    private _thenTemplateRef: TemplateRef<PermissionIfContext> | null = null;
+    private _elseTemplateRef: TemplateRef<PermissionIfContext> | null = null;
+    private _thenViewRef: EmbeddedViewRef<PermissionIfContext> | null = null;
+    private _elseViewRef: EmbeddedViewRef<PermissionIfContext> | null = null;
 
     constructor(
-        private templateRef: TemplateRef<any>,
+        templateRef: TemplateRef<any>,
         private viewContainer: ViewContainerRef,
         private authorizer: Authorization
     ) {
+        this._thenTemplateRef = templateRef;
+        this.setExternalCondition(true);
+        this.setCondition(false);
     }
 
     @Input()
     set permissionIf(perm: string | RawPermissionMap) {
         this.rawMap = getRawMap(perm);
+    }
+
+    @Input()
+    set permissionIfExternal(condition: boolean) {
+        this.setExternalCondition(condition);
+        this.setCondition();
+    }
+
+    @Input()
+    set permissionIfThen(templateRef: TemplateRef<PermissionIfContext>) {
+        this._thenTemplateRef = templateRef;
+        this._thenViewRef = null;  // clear previous view if any.
+        this.updateView();
+    }
+
+    @Input()
+    set permissionIfElse(templateRef: TemplateRef<PermissionIfContext>) {
+        this._elseTemplateRef = templateRef;
+        this._elseViewRef = null;  // clear previous view if any.
+        this.updateView();
     }
 
     ngOnInit() {
@@ -36,18 +63,45 @@ export class PermissionIfDirective implements OnInit, OnChanges {
 
     check() {
         this.authorizer.resolve(this.authorizer.genPermMap(this.rawMap))
-            .takeWhile(result => result[0] !== this.$state)
             .subscribe((result) => {
-                this.$state = result[0];
+                this.setCondition(result[0]);
                 this.updateView();
             });
     }
 
     updateView() {
-        if (this.$state) {
-            this.viewContainer.createEmbeddedView(this.templateRef);
+        if (this._context.$implicit) {
+            if (!this._thenViewRef) {
+                this.viewContainer.clear();
+                this._elseViewRef = null;
+                if (this._thenTemplateRef) {
+                    this._thenViewRef =
+                        this.viewContainer.createEmbeddedView(this._thenTemplateRef, this._context);
+                }
+            }
         } else {
-            this.viewContainer.clear();
+            if (!this._elseViewRef) {
+                this.viewContainer.clear();
+                this._thenViewRef = null;
+                if (this._elseTemplateRef) {
+                    this._elseViewRef =
+                        this.viewContainer.createEmbeddedView(this._elseTemplateRef, this._context);
+                }
+            }
         }
     }
+
+    private setCondition(condition = true) {
+        this._context.$implicit = this._context.permissionIf = condition && this._context.external;
+    }
+
+    private setExternalCondition(condition: boolean) {
+        this._context.external = condition;
+    }
+}
+
+export class PermissionIfContext {
+    public $implicit: boolean = null;
+    public permissionIf: boolean = null;
+    public external: boolean = null;
 }
