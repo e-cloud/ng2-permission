688c53fae5488109236f1595adbef3eb8256ca21
e-cloud <saintscott119@gmail.com>
2017-08-01 22:20:00 +0800

    fix(permission-pipe): keep data updated from store changes

--- src/permission/pipes/permission.pipe.ts
+++ src/permission/pipes/permission.pipe.ts
@@ -1,5 +1,6 @@
 import { OnDestroy, Pipe, PipeTransform } from '@angular/core';
 import { BehaviorSubject } from 'rxjs/BehaviorSubject';
+import { Subscription } from 'rxjs/Subscription';
 import { Authorization } from '../Authorization/Authorization';
 import { RawPermissionMap } from '../Authorization/PermissionMap';
 import { getRawMap } from '../utils';
@@ -11,27 +12,37 @@ import { getRawMap } from '../utils';
 export class PermissionPipe implements OnDestroy, PipeTransform {
     private permission$ = new BehaviorSubject(false);
     private lastInput: any;
+    private permissionChanges$: Subscription;
 
-    constructor(private authorizer: Authorization) {}
+    constructor(private authorizer: Authorization) {
+        this.permissionChanges$ = this.authorizer.onChanges()
+            .subscribe(() => {
+                this.checkPermission(this.lastInput);
+            });
+    }
 
     ngOnDestroy(): void {
         if (this.permission$) {
             this.permission$.unsubscribe();
         }
+        this.permissionChanges$.unsubscribe();
     }
 
     transform(value: string | RawPermissionMap): any {
         if (value !== this.lastInput) {
-            this.authorizer.resolveRaw({ ...getRawMap(value), redirectTo: null })
-                .take(1)
-                .subscribe((result) => {
-                    this.permission$.next(result[0]);
-                });
+            this.checkPermission(value);
+            this.lastInput = value;
         }
 
-        this.lastInput = value;
-
         return this.permission$;
+    }
+
+    private checkPermission(value: string | RawPermissionMap) {
+        this.authorizer.resolveRaw({ ...getRawMap(value), redirectTo: null })
+            .take(1)
+            .subscribe((result) => {
+                this.permission$.next(result[0]);
+            });
     }
 
 }
